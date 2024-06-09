4587fd8d599a30c7b386397139dfbc8b731853a7
A1D2D <79410999+A1D2D@users.noreply.github.com>
2024-06-09 21:44:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
-#include <vector>
 #include <iostream>
-#include <string>
-
-#include "KeyWordSearch.h"
 #include "ExpressionParser.h"
 
 int main() {
+   //* This is an example project to demonstrate what the Header is capable,
+   //* the program just expects a value and on entering an expression(calculation)
+   //* it returns the calculated value also it can take in Variables.
+   //* The parser also constructs an order of operations tree that can be useful,
+   //* if u want to make a programing language or something.
+
+   float PI = 3.14;
 
    ExpressionParser<float> parser;
-   parser.buildTree("x*y*2-8",{"x","y"});
-   std::cout << parser.evaluate({2,4});
 
-   /*
-   There are some problems the -5 or -var
-   dose not work. So if u want to work with negative numbers
-   just do (0-x) or (0-78) that should work fine.
+   std::cout << "write !stop to exit" << std::endl;
 
-   The other issue is if the to variables contain each other 
-   I mean do not create a value and a val variable, becouse value contains val.
-   */
-};
+   std::string inputStr;
+   while(inputStr != "!stop") {
+      std::cout << "input a expression also u can use PI" << std::endl;
+      std::cin >> inputStr;
+
+      parser.buildTree(inputStr, {"PI"});
+      float value = parser.evaluate({PI});
+      parser.deleteTree();
+
+      std::cout << "Value: " << value << std::endl;
+   }
+   return 0;
+}
